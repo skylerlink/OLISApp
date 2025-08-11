ba9d3a836c7a460c7d1cd5307f7be9661b889047
Skyler Link <slink@vargomail.com>
2025-08-11 07:55:45 -0400

    first commit

---
+++ src/main/java/com/skylerlink/OLISApp/OlisAppApplication.java
@@ -0,0 +1,13 @@
+package com.skylerlink.OLISApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlisAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OlisAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skylerlink/OLISApp/OlisAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skylerlink.OLISApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlisAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
